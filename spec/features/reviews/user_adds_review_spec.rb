e36f093399d48924010bfccbf4f307bc0a093fd3
Evan Watterud <cod.videos@hotmail.com>
2016-05-10 17:14:51 -0400

    updated add review tests and functionality

--- spec/features/reviews/user_adds_review_spec.rb
+++ spec/features/reviews/user_adds_review_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
 feature 'user adds a review to a beer' do
+  before(:each) do
+    @beer = FactoryGirl.create(:beer)
+  end
+
   scenario 'authenticated user adds a review to a beer' do
     user = log_in
-    beer = FactoryGirl.create(:beer)
-    visit beer_path(beer)
+    visit beer_path(@beer)
 
     fill_in "review_body", with: 'something'
     click_button 'Add Review'
 
-    expect(current_path).to eq(beer_path(beer))
+    expect(current_path).to eq(beer_path(@beer))
     expect(page).to have_button('Add Review')
-    expect(beer.reviews.length).to eq(1)
+    expect(@beer.reviews.length).to eq(1)
+  end
+
+  scenario 'unauthenticated user can not add reviews' do
+    visit beer_path(@beer)
+
+    expect(page).to have_button('Add Review', disabled: true)
+    expect(page).to have_field('review_body', disabled: true)
+    within('.review_box') do
+      expect(page).to have_content('Log in to add reviews')
+    end
   end
 end
